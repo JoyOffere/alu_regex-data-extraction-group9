13d7843db28fe550b36f2830c901fef3921a1c8e
Bavukile <b.vilane@alustudent.com>
2023-10-03 13:10:15 -0700

    New API sample

--- Social_media_usernames.py
+++ Social_media_usernames.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import re
 
-def extract_usernames(API_responses):
+def extract_usernames(API_sample):
     pattern = r'@([a-zA-Z0-9_]+)'
-    matches = re.findall(pattern, API_responses)
+    matches = re.findall(pattern, API_sample)
     return matches
 
 if __name__ == "__main__":
-    API_responses = "Check out this cool post by @user123 and follow @user789"
-    usernames = extract_usernames(API_responses)
+    API_sample = "Dec 25, 2023 - 02:30 PM, Dec 31, 2023 - 00:00 AM, Oct 01, 2024 - 04:00 PM, Oct 03, 2025 - 00:00 AM, rgb(255, 0, 128), rgb(0, 255, 0), rgb(255, 255, 255), rgb(210, 210), rgb(0, 0, 0), Headline: Breaking News - Important Announcement, Headline: Latest Updates - Weather Forecast, Headline: Sports News - Exciting Match Results, ABC123, JOY010, FAITH112, ACE247,  EYE001, big101, @user123, @user789..."
+    usernames = extract_usernames(API_sample)
     print("Usernames:", usernames)
 
