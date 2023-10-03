6be18addfac81e4a34cb342b2067f92f6a84a40f
GChukwudi <g.chukwudi@alustudent.com>
2023-10-03 08:40:35 -0700

    updated RGB_color.py file

--- RGB_color.py
+++ RGB_color.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import re
 
-API_sample = "This is a color: rgb(255, 0, 128), rgb(0, 255, 0), rgb(255, 255, 255), rgb(210, 210) and rgb(0, 0, 0)"
+API_sample = "This is a color: rgb(255, 0, 128), rgb(0, 255, 0), rgb(255, 255, 255), rgb(210, 210), rgb(0, 0, 0)..."
 pattern = r"rgb\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})\)"
 
 matches = re.findall(pattern, API_sample)
