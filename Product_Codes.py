d1cfc7bd156d80e7eb47ea5297533fb3ad9b37e7
Joy Offere <j.offere@alustudent.com>
2023-10-03 21:51:20 +0200

    Product Codes File

--- Product_Codes.py
+++ Product_Codes.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 import re
 Product_Codes_API = "ABC123, JOY010, FAITH112, ACE247,  EYE001, big101..."
+Pattern = r"\b[A-Z]{3}\d{3}\b"
+matches = re.findall(Pattern,Product_Codes_API)
+for i in matches:
+    print(i)
