bbe090aa6726744c7a86be731daeb1158e31c9fa
Bavukile <b.vilane@alustudent.com>
2023-10-03 12:59:35 -0700

    updated code

--- Social_media_usernames.py
+++ Social_media_usernames.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import re
 
-def extract_usernames(text):
+def extract_usernames(API_responses):
     pattern = r'@([a-zA-Z0-9_]+)'
-    matches = re.findall(pattern, text)
+    matches = re.findall(pattern, API_responses)
     return matches
 
 if __name__ == "__main__":
-    text = "Check out this cool post by @user123 and follow @example_user!"
-    usernames = extract_usernames(text)
+    API_responses = "Check out this cool post by @user123 and follow @example_user!"
+    usernames = extract_usernames(API_responses)
     print("Usernames:", usernames)
 
