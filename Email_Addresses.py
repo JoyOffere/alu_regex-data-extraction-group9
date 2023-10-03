de26aa5ab39faa4255227c6691ce9748854ed7e2
Chidiebere Ekwedike <c.ekwedike@alustudent.com>
2023-10-04 00:28:52 +0200

    text doc added

--- Email_Addresses.py
+++ Email_Addresses.py
@@ -1,2 +1,17 @@
 import re
 
+# Read text from an external text file
+file_path = 'text-doc.txt'
+
+try:
+    with open(file_path, 'r') as file:
+        text = file.read()
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        email_addresses = re.findall(email_pattern, text)
+
+        # Print the extracted email addresses
+        for email in email_addresses:
+            print(email)
+    
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
