a688e97b831e66a795b75078f45aa1b75ee2cd6f
GChukwudi <g.chukwudi@alustudent.com>
2023-10-04 03:00:31 -0700

    updated gile

--- Ingredient_lists.py
+++ Ingredient_lists.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import re
 
-API_sample = "Dec 25, 2023 - 02:30 PM, Dec 31, 2023 - 00:00 AM, Oct 01, 2024 - 04:00 PM, Oct 03, 2025 - 00:00 AM, rgb(255, 0, 128), rgb(0, 255, 0), rgb(255, 255, 255), rgb(210, 210), rgb(0, 0, 0), Headline: Breaking News - Important Announcement, Headline: Latest Updates - Weather Forecast, Headline: Sports News - Exciting Match Results, ABC123, JOY010, FAITH112, ACE247,  EYE001, big101, @user123, @user789..."
+API_sample = "Dec 25, 2023 - 02:30 PM, Dec 31, 2023 - 00:00 AM, Oct 01, 2024 - 04:00 PM, Oct 03, 2025 - 00:00 AM, rgb(255, 0, 128), rgb(0, 255, 0), rgb(255, 255, 255), rgb(210, 210), rgb(0, 0, 0), tomato, fish, onion, garlic, salt, Headline: Breaking News - Important Announcement, Headline: Latest Updates - Weather Forecast, Headline: Sports News - Exciting Match Results, ABC123, JOY010, FAITH112, ACE247,  EYE001, big101, @user123, @user789, ..."
 
-ingredients_pattern = r"'(.*?)'"
-ingredients_matches = re.findall(ingredients_pattern, raw_data)
-
-for ingredients in ingredients_matches:
-    ingredients_list = [ingredient.strip() for ingredient in ingredients.split(',')]
-    print("Ingredients:", ingredients_list)
+pattern = r"Ingredients:\s*(.*?)\s*$"
+matches = re.findall(pattern, API_sample, re.MULTILINE)
+I_list = [ingredient.strip() for ingredient in matches[0].split(',')]
+print(I_list)
--- News_headlines.py
+++ News_headlines.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import re
 
-API_data = """ Headline: Breaking News - Important Announcement, Headline: Latest Updates - Weather Forecast, Headline: Sports News - Exciting Match Results...."""
+API_data = "Dec 25, 2023 - 02:30 PM, Dec 31, 2023 - 00:00 AM, Oct 01, 2024 - 04:00 PM, Oct 03, 2025 - 00:00 AM, rgb(255, 0, 128), rgb(0, 255, 0), rgb(255, 255, 255), rgb(210, 210), rgb(0, 0, 0), Headline: Breaking News - Important Announcement, Headline: Latest Updates - Weather Forecast, Headline: Sports News - Exciting Match Results, ABC123, JOY010, FAITH112, ACE247,  EYE001, big101, @user123, @user789..."
 
 
 pattern = r'Headline: (.*?) - (.*?)'
 
-matches = re.finditer(pattern, raw_data)
+matches = re.finditer(pattern, API_data)
 
 structured_headlines = []
 
@@ -14,5 +14,5 @@ for match in matches:
     headline = match.group(1)
     subheader = match.group(2)
     structured_headlines.append({"Headline": headline, "Subheader": subheader})
-for item in structured_headlines:
-    print(item)
+    for item in structured_headlines:
+        print(item)
