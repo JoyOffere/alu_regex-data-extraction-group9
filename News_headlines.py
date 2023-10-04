1cdd04577b4baeb8c3ac73c5b00e715a75ede44d
GChukwudi <g.chukwudi@alustudent.com>
2023-10-04 04:05:12 -0700

    updated task 6

--- News_headlines.py
+++ News_headlines.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import re
 
-API_data = "Dec 25, 2023 - 02:30 PM, Dec 31, 2023 - 00:00 AM, Oct 01, 2024 - 04:00 PM, Oct 03, 2025 - 00:00 AM, rgb(255, 0, 128), rgb(0, 255, 0), rgb(255, 255, 255), rgb(210, 210), rgb(0, 0, 0), Headline: Breaking News - Important Announcement, Headline: Latest Updates - Weather Forecast, Headline: Sports News - Exciting Match Results, ABC123, JOY010, FAITH112, ACE247,  EYE001, big101, @user123, @user789..."
-
-
+#raw API response containing news headlines
+raw_data = """Here are some news headlines:
+    Headline: Breaking News - Important Announcement
+    Headline: Latest Updates - Weather Forecast
+    Headline: Sports News - Exciting Match Results
+    """
 pattern = r'Headline: (.*?) - (.*?)'
-
-matches = re.finditer(pattern, API_data)
-
+matches = re.finditer(pattern, raw_data)
 structured_headlines = []
 
 for match in matches:
